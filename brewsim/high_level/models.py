634f540c3deb09a00b191e8d67c9ab487ec16ef2
DovahkiinBR <arthur.nelias@gmail.com>
2023-09-27 11:29:25 +0200

    add high_level models & admin

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -1,1 +1,53 @@
-# Create your models here.
+from django.db import models
+
+
+class Ingredient(models.Model):
+    nom = models.CharField(max_length=50)
+
+
+class Departement(models.Model):
+    numero = models.IntegerField(MinValueValidator=1, MaxValueValidator=100)
+    prixm2 = models.IntegerField(MinValueValidator=1)
+
+
+class Prix(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
+    prix = models.IntegerField(MinValueValidator=0)
+
+
+class QuantiteIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    quantite = models.IntegerField(MinValueValidator=0)
+
+
+class Machine(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.IntegerField(MinValueValidator=0)
+
+
+class Action(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    commande = models.CharField(max_length=50)
+    duree = models.IntegerField(MinValueValidator=0)
+    action = models.ForeignKey(
+        "self",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        # related_name"+",
+    )
+
+
+class Recette(models.Model):
+    nom = models.CharField(max_length=50)
+    action = models.ForeignKey(Action, on_delete=models.PROTECT)
+
+
+class Usine(models.Model):
+    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
+    taille = models.IntegerField(MinValueValidator=1)
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    recettes = models.ForeignKey(Recette, on_delete=models.PROTECT)
+    stocks = models.IntegerField(MinValueValidator=0)
