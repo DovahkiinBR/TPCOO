91ab7745333f203d6a3c1e2fb4fc5ed457fd6d1b
DovahkiinBR <arthur.nelias@gmail.com>
2023-09-28 14:25:43 +0200

    add interface

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Ingredient(models.Model):
     nom = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nom
+
 
 class Departement(models.Model):
     numero = models.IntegerField()
