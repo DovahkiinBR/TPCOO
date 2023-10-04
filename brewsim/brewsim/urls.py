8b0bff230755359b8b928a9f66efc6e237df7e8b
DovahkiinBR <arthur.nelias@gmail.com>
2023-10-04 10:01:34 +0200

    Implementation JSON

--- brewsim/brewsim/urls.py
+++ brewsim/brewsim/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from high_level import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "departement/<int:pk>",
+        views.DepartementDetailView.as_view(),
+        name="departement",
+    ),
 ]
--- brewsim/high_level/views.py
+++ brewsim/high_level/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from json import dumps
+
+from django.http import HttpResponse
+from django.views.generic import DetailView
+
+from .models import Departement
+
+
+class DepartementDetailView(DetailView):
+    model = Departement
+
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
